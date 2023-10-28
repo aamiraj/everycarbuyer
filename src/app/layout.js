5992af8590fb79c7d00131e71aa4cb78710b97af
Abdullah Miraj <miraj2465@gmail.com>
2023-10-28 23:44:34 +0600

    resolved issue in layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,35 +1,4 @@
 import "./globals.css";
-import localFont from "next/font/local";
-
-// const br_omega = localFont({
-//   src: [
-//     {
-//       path: "../BROmega/BROmega-Regular.woff2",
-//       weight: "400",
-//       style: "normal",
-//     },
-//     {
-//       path: "../BROmega/BROmega-Light.woff2",
-//       weight: "300",
-//       style: "normal",
-//     },
-//     {
-//       path: "../BROmega/BROmega-Medium.woff2",
-//       weight: "500",
-//       style: "normal",
-//     },
-//     {
-//       path: "../BROmega/BROmega-SemiBold.woff2",
-//       weight: "600",
-//       style: "normal",
-//     },
-//     {
-//       path: "../BROmega/BROmega-Bold.woff2",
-//       weight: "700",
-//       style: "normal",
-//     },
-//   ],
-// });
 
 export const metadata = {
   title: "everycarbuyer | Your car, Your price Selling made simple",
@@ -40,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="font-bromega">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
